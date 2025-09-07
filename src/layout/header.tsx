cb10fcdbc081ecf45da345fd44fe4f1926f8e1b0
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-09-07 16:43:29 -0300

    refactor: move padding to each layout component and make root flex

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from '../components/theme-toggle';
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center gap-5 justify-between md:flex-row md:gap-0">
+    <header className="flex flex-col items-center gap-5 justify-between px-8 py-5 md:flex-row md:gap-0">
       <div className="flex flex-col items-center gap-5 md:flex-row md:gap-7">
         <h1 className="underline decoration-amber-400 decoration-2 underline-offset-4 font-semibold text-3xl">
           ArtQueue
