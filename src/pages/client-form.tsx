0e17ecbb9b7f21a8ed863cad5f8cb1468976d5c8
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-09-27 16:20:09 -0300

    fix: adjust form progress bar

--- src/pages/client-form.tsx
+++ src/pages/client-form.tsx
@@ -28,18 +28,17 @@ export function ClientFormPage() {
       <Header />
 
       <CommissionsContainer>
-        <form
-          className="w-full p-6 flex flex-col gap-6 bg-white border border-b-4 border-e-4 rounded-3xl overflow-hidden dark:bg-zinc-800 dark:border-white"
-        >
-          <div className="-mt-6 -ms-6 w-7/12 h-1 bg-amber-400"></div>
+        <div className="w-full flex flex-col bg-white border border-b-4 border-e-4 rounded-3xl overflow-hidden dark:bg-zinc-800 dark:border-white">
+          <div className="w-full h-1 bg-black/10 dark:bg-white/20">
+            <div className="w-1/2 h-full bg-amber-400 transition-all duration-300"></div>
+          </div>
 
-          <header className="flex flex-col gap-2">
+          <header className="px-6 mt-6 flex flex-col gap-1">
             <h3 className="text-xl font-medium">Nova comissão</h3>
-
             <p className="text-black/70 dark:text-white/70">Preencha as informações sobre seu cliente.</p>
           </header>
 
-          <main className="flex flex-col gap-3">
+          <form className="px-6 mt-3 mb-6 flex flex-col gap-3">
             <div className="flex flex-col gap-2">
               <p>Nome do cliente</p>
               <input
@@ -51,7 +50,7 @@ export function ClientFormPage() {
                 onChange={(e) => {
                   setCommission({ ...commission, name: e.target.value });
                 }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               />
             </div>
 
@@ -63,29 +62,29 @@ export function ClientFormPage() {
                 placeholder="joaodasartes123"
                 value={commission.twitter}
                 onChange={(e) => { setCommission({ ...commission, twitter: e.target.value }); }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               />
             </div>
-          </main>
 
-          <div className="flex gap-6 justify-between">
-            <button
-              type="reset"
-              onClick={onBackButton}
-              className="flex gap-2 border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:bg-black/20 hover:cursor-pointer hover:rounded-none"
-            >
-              <ArrowLeftIcon />
-              Voltar
-            </button>
+            <div className="mt-3 flex gap-6 justify-between">
+              <button
+                type="reset"
+                onClick={onBackButton}
+                className="flex gap-2 border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:bg-black/20 hover:cursor-pointer hover:rounded-none"
+              >
+                <ArrowLeftIcon />
+                Voltar
+              </button>
 
-            <button
-              onClick={onNextButton}
-              className="flex gap-2 justify-center w-full border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all bg-amber-400 hover:cursor-pointer hover:rounded-none dark:text-black dark:border-white"
-            >
-              Continuar
-            </button>
-          </div>
-        </form>
+              <button
+                onClick={onNextButton}
+                className="flex gap-2 justify-center w-full border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all bg-amber-400 hover:cursor-pointer hover:rounded-none dark:text-black dark:border-white"
+              >
+                Continuar
+              </button>
+            </div>
+          </form>
+        </div>
       </CommissionsContainer>
 
       <Footer />
--- src/pages/commission-form.tsx
+++ src/pages/commission-form.tsx
@@ -33,18 +33,17 @@ export function CommissionFormPage() {
       <Header />
 
       <CommissionsContainer>
-        <form
-          className="w-full p-6 flex flex-col gap-6 bg-white border border-b-4 border-e-4 rounded-3xl overflow-hidden dark:bg-zinc-800 dark:border-white"
-        >
-          <div className="-mt-6 self-end -me-6 w-7/12 h-1 bg-amber-400"></div>
+        <div className="w-full flex flex-col bg-white border border-b-4 border-e-4 rounded-3xl overflow-hidden dark:bg-zinc-800 dark:border-white">
+          <div className="w-full h-1 bg-black/10 dark:bg-white/20">
+            <div className="w-full h-full bg-amber-400 transition-all duration-300"></div>
+          </div>
 
-          <header className="flex flex-col gap-2">
+          <header className="px-6 mt-6 flex flex-col gap-1">
             <h3 className="text-xl font-medium">Nova comissão</h3>
-
             <p className="text-black/70 dark:text-white/70">Preencha as informações sobre a encomenda.</p>
           </header>
 
-          <main className="flex flex-col gap-3">
+          <form className="px-6 mt-3 mb-6 flex flex-col gap-3">
             <div className="flex flex-col gap-2">
               <p>Preço (R$)</p>
               <input
@@ -71,7 +70,7 @@ export function CommissionFormPage() {
                   WebkitAppearance: 'none',
                   MozAppearance: 'textfield',
                 }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               />
             </div>
 
@@ -84,7 +83,7 @@ export function CommissionFormPage() {
                 onChange={(e) => {
                   setCommission({ ...commission, paymentStatus: e.target.value as 'not_paid' | 'half_paid' | 'paid' });
                 }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               >
                 <option value="not_paid">Não pagou</option>
                 <option value="half_paid">Pagou metade</option>
@@ -101,7 +100,7 @@ export function CommissionFormPage() {
                 onChange={(e) => {
                   setCommission({ ...commission, stage: e.target.value as 'in_queue' | 'sketch' | 'line' | 'paint' | 'finished' });
                 }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               >
                 <option value="in_queue">Na fila</option>
                 <option value="sketch">Esboço</option>
@@ -120,29 +119,29 @@ export function CommissionFormPage() {
                 onChange={(e) => {
                   setCommission({ ...commission, deadlineDate: new Date(e.target.value) });
                 }}
-                className="border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
+                className="border-1 rounded-lg px-4 py-2 border-b-2 border-e-2 transition-all hover:cursor-pointer hover:rounded-none focus:rounded-none focus:outline-none focus:border-blue-700"
               />
             </div>
-          </main>
 
-          <div className="flex gap-6 justify-between">
-            <button
-              type="reset"
-              onClick={onBackButton}
-              className="flex gap-2 border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:bg-black/20 hover:cursor-pointer hover:rounded-none"
-            >
-              <ArrowLeftIcon />
-              Voltar
-            </button>
+            <div className="mt-3 flex gap-6 justify-between">
+              <button
+                type="reset"
+                onClick={onBackButton}
+                className="flex gap-2 border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all hover:bg-black/20 hover:cursor-pointer hover:rounded-none"
+              >
+                <ArrowLeftIcon />
+                Voltar
+              </button>
 
-            <button
-              onClick={onSubmitButton}
-              className="w-full border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all bg-amber-400 hover:cursor-pointer hover:rounded-none dark:text-black dark:border-white"
-            >
-              Concluir
-            </button>
-          </div>
-        </form>
+              <button
+                onClick={onSubmitButton}
+                className="w-full border-1 rounded-lg p-2 border-b-2 border-e-2 transition-all bg-amber-400 hover:cursor-pointer hover:rounded-none dark:text-black dark:border-white"
+              >
+                Concluir
+              </button>
+            </div>
+          </form>
+        </div>
       </CommissionsContainer>
 
       <Footer />
